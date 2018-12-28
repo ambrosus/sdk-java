51946af4fe135f2221e7c834a6f7865c2aeab824
Dzmitry Kavaliou <dzmitry.kavaliou@gmail.com>
2018-12-28 15:04:33 +0300

    Improving Asset model

--- sdk/src/main/java/com/ambrosus/sdk/models/Asset.java
+++ sdk/src/main/java/com/ambrosus/sdk/models/Asset.java
@@ -1,13 +1,79 @@
 package com.ambrosus.sdk.models;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
 import java.io.Serializable;
 
 public class Asset implements Serializable {
 
     private String assetId;
-    private String name;
 
+    private Content content;
+    private MetaData metadata;
+
+    @NonNull
     public String getSystemId() {
         return assetId;
+    }
+
+    @Nullable
+    public String getName() {
+        return content.idData.name;
+    }
+
+    @NonNull
+    public String getAccount() {
+        return content.idData.createdBy;
+    }
+
+    @NonNull
+    public long getSequenceNumber() {
+        return content.idData.sequenceNumber;
+    }
+
+    @NonNull
+    public long getTimestamp() {
+        return content.idData.timestamp;
+    }
+
+    @NonNull
+    public MetaData getMetaData() {
+        return metadata;
+    }
+
+
+    private static class Content implements Serializable {
+        private IDData idData;
+    }
+
+    private static class IDData implements Serializable {
+
+        private String name;
+        private String createdBy;
+        private long timestamp;
+        private long sequenceNumber;
+
+    }
+
+    public static class MetaData implements Serializable {
+        private String bundleTransactionHash;
+        private String bundleUploadTimestamp;
+        private String bundleId;
+
+        @NonNull
+        public String getBundleTransactionHash() {
+            return bundleTransactionHash;
+        }
+
+        @NonNull
+        public String getBundleUploadTimestamp() {
+            return bundleUploadTimestamp;
+        }
+
+        @NonNull
+        public String getBundleId() {
+            return bundleId;
+        }
     }
 }
