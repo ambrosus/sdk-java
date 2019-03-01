e93a75edb404bb04477553e3cc73bad64864a2bd
Dzmitry Kavaliou <dzmitry.kavaliou@gmail.com>
2019-03-01 19:50:56 +0300

    Minor code improvement

--- sdk/src/test/java/com/ambrosus/sdk/model/AMBEventIntegrationTest.java
+++ sdk/src/test/java/com/ambrosus/sdk/model/AMBEventIntegrationTest.java
@@ -47,24 +47,20 @@ public class AMBEventIntegrationTest {
     }
 
     @Test
-    public void findEventsSimpleTest(){
+    public void findEventsSimpleTest() throws Throwable {
         final String expectedAssetID = "0xfe7d80686adf18b2259a60d836104ab866a20b6eabf3bc4cde75ffc4aa8015a0";
 
         AMBEventQueryBuilder searchParamsBuilder = new AMBEventQueryBuilder().byDataObjectIdentifier(Identifier.EAN13, "6942507312009");
 
         NetworkCall<SearchResult<Event>> networkCall = network.findEvents(searchParamsBuilder.build());
 
-        try {
-            SearchResult<Event> result = networkCall.execute();
-            for (Event event : result.getValues()) {
-                if(expectedAssetID.equals(event.getAssetId()))
-                    return;
-            }
-            fail("Wasn't able to find event which has specified identifier and belongs to asset with ID: " + expectedAssetID);
-            System.out.println();
-        } catch (Throwable t) {
-            throw new RuntimeException(t);
+        SearchResult<Event> result = networkCall.execute();
+        for (Event event : result.getValues()) {
+            if(expectedAssetID.equals(event.getAssetId()))
+                return;
         }
+        fail("Wasn't able to find event which has specified identifier and belongs to asset with ID: " + expectedAssetID);
+        System.out.println();
     }
 
     @Test
