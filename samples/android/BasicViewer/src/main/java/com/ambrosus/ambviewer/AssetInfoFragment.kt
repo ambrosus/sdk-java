d10cce950b04f9c110d5e1306a29c5dead785bdb
Dzmitry Kavaliou <dzmitry.kavaliou@gmail.com>
2019-04-23 18:21:25 +0300

    AssetInfo fragment UI and logic implementation + extracting common style elements

--- samples/android/BasicViewer/src/main/java/com/ambrosus/ambviewer/AssetInfoFragment.kt
+++ samples/android/BasicViewer/src/main/java/com/ambrosus/ambviewer/AssetInfoFragment.kt
@@ -7,7 +7,12 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import com.ambrosus.sdk.Asset
+import com.ambrosus.sdk.model.AMBAssetInfo
+import com.google.gson.JsonObject
+import kotlinx.android.synthetic.main.fragment_asset_info.*
+import java.text.SimpleDateFormat
+import java.util.Date
 
 
 class AssetInfoFragment : Fragment() {
@@ -16,4 +21,30 @@ class AssetInfoFragment : Fragment() {
                               savedInstanceState: Bundle?): View?
         = inflater.inflate(R.layout.fragment_asset_info, container, false)
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val asset = ARG_ASSET_DATA.get(this)
+        //TODO: change to X days ago
+        creationDate.text = SimpleDateFormat("D, MMM").format(asset.timestamp)
+        createdBy.text = asset.accountAddress
+        when(asset) {
+            is AMBAssetInfo -> {
+                assetName.text = asset.name
+                assetID.text = asset.assetId
+
+                val productInformation = asset.attributes["Product Information"]
+                detailsDescription.text = when(productInformation) {
+                    is JsonObject -> productInformation.asJsonObject.get("Description")?.asString
+                    else -> {
+                        productInformation?.asString
+                    }
+                }
+            }
+            is Asset -> {
+                assetName.text = getString(R.string.txtNoName)
+                assetID.text = asset.systemId
+            }
+        }
+    }
 }
